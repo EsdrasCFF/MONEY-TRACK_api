8d61101c203d0d8c75a7c2b4ccb053b3aa781349
Esdras Castro <ecff.dev@gmail.com>
2024-08-18 22:49:16 -0300

    chore: add simple-import-sort plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import pluginJs from '@eslint/js'
 import tseslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import prettierPlugin from 'eslint-plugin-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 // Configuração Flat
 export default [
@@ -16,6 +17,7 @@ export default [
     plugins: {
       '@typescript-eslint': tseslint,
       prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
