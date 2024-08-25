b43c1a6f3413cad9334fdd4ab5a853b9e9c97923
Esdras Castro <ecff.dev@gmail.com>
2024-08-25 18:15:44 -0300

    feat: add createUser route and registered on server

---
+++ src/routes/users/create-user.ts
@@ -0,0 +1,59 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { CreateUserController } from '../../controllers/create-user'
+import { CreateUserRepository } from '../../repositories/users/create-user'
+import { GetUserByEmailRepository } from '../../repositories/users/get-user-by-email'
+import { CreateUserService } from '../../services/users/create-user'
+
+export interface WebHookData {
+  data: {
+    id: string
+    first_name: string
+    last_name: string
+    email_addresses: [
+      {
+        email_address: string
+      },
+    ]
+  }
+  object: string
+  type: string
+}
+
+export async function createUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/api/users',
+    {
+      schema: {
+        response: {
+          201: z.object({
+            data: z.object({
+              id: z.string(),
+              firstName: z.string(),
+              lastName: z.string(),
+              email: z.string(),
+              document: z.string().optional().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const json = request.body
+
+      const createUserRepository = new CreateUserRepository()
+      const getUserByEmailRepository = new GetUserByEmailRepository()
+
+      const createUserService = new CreateUserService(createUserRepository, getUserByEmailRepository)
+
+      const createUserController = new CreateUserController(createUserService)
+      const user = await createUserController.execute(json as WebHookData, request)
+
+      return reply.code(201).send({
+        data: user,
+      })
+    }
+  )
+}
