f8c0f55f3a200e6708a1ab47d6402d1c48c9d5f2
Esdras Castro <ecff.dev@gmail.com>
2024-08-26 09:20:17 -0300

    feat: add CreateAccountController

---
+++ src/controllers/accounts/create-account.ts
@@ -0,0 +1,34 @@
+import { Account } from '@prisma/client'
+
+import { CreateAccountProps } from '../../repositories/accounts/create-account'
+import { BadRequest, Unauthorized } from '../../routes/_errors/erros-instance'
+import { ICreateAccountService } from '../../services/accounts/create-account'
+
+export interface ICreateAccountController {
+  execute(createAccountParams: CreateAccountProps, authenticatedUserId: string | undefined): Promise<Account>
+}
+
+export class CreateAccountController implements ICreateAccountController {
+  constructor(private createAccountService: ICreateAccountService) {}
+
+  async execute({ name, userId }: CreateAccountProps, authenticatedUserId: string | undefined) {
+    if (!authenticatedUserId) {
+      throw new Unauthorized('Unauthenticated!')
+    }
+
+    if (!name || !userId) {
+      throw new BadRequest('Provided Params are not valid!')
+    }
+
+    if (userId != authenticatedUserId) {
+      throw new BadRequest('Provided userId is not valid!')
+    }
+
+    const createdAccount = await this.createAccountService.execute({
+      name,
+      userId,
+    })
+
+    return createdAccount
+  }
+}
