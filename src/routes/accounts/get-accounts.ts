51cc95f042cfc6658e57b9950ae6140c3fcc3606
Esdras Castro <ecff.dev@gmail.com>
2024-08-30 15:06:12 -0300

    feat: add GetAccounts api route

---
+++ src/routes/accounts/get-accounts.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { GetAccountsByUserIdController } from '@/controllers/accounts/get-accounts-by-userId'
+import { GetAccountsByUserIdRepository } from '@/repositories/accounts/get-accounts-by-userId'
+import { GetUserByIdRepository } from '@/repositories/users/get-user-by-id'
+import { GetAccountsByUserIdService } from '@/services/accounts/get-accounts-by-userId'
+
+export const getAccountsSchema = z.object({
+  Authorization: z.string({ required_error: 'Unauthorized' }),
+})
+
+export async function getAccounts(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/api/accounts',
+    {
+      schema: {
+        // headers: z.o,
+        response: {
+          200: z.object({
+            data: z.array(
+              z.object({
+                id: z.string(),
+                name: z.string(),
+                userId: z.string(),
+              })
+            ),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const id = request.user!.id
+
+      const getAccountsByUserIdRespository = new GetAccountsByUserIdRepository()
+      const getUserByIdRepository = new GetUserByIdRepository()
+
+      const getAccountsByUserIdService = new GetAccountsByUserIdService(getAccountsByUserIdRespository, getUserByIdRepository)
+
+      const getAccountsByUserIdController = new GetAccountsByUserIdController(getAccountsByUserIdService)
+
+      const accounts = await getAccountsByUserIdController.execute(id)
+
+      return reply.code(200).send({
+        data: accounts,
+      })
+    }
+  )
+}
