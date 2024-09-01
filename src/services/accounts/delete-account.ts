70ca2653d984136227074ecdf3a07143619bc836
Esdras Castro <ecff.dev@gmail.com>
2024-09-01 15:39:59 -0300

    feat: add DeleteAccountService

---
+++ src/services/accounts/delete-account.ts
@@ -0,0 +1,32 @@
+import { Account } from '@prisma/client'
+
+import { IDeleteAccountRepository } from '@/repositories/accounts/delete-account'
+import { IGetAccountByIdRepository } from '@/repositories/accounts/get-account-by-id'
+import { BadRequest, NotFound } from '@/routes/_errors/errors-instance'
+
+export interface IDeleteAccountService {
+  execute(accountId: string, userId: string): Promise<Account>
+}
+
+export class DeleteAccountService implements IDeleteAccountService {
+  constructor(
+    private getAccountByIdRepository: IGetAccountByIdRepository,
+    private deleteAccountRepository: IDeleteAccountRepository
+  ) {}
+
+  async execute(accountId: string) {
+    const accountExists = await this.getAccountByIdRepository.execute(accountId)
+
+    if (!accountExists) {
+      throw new NotFound('Account not found')
+    }
+
+    const deletedAccount = await this.deleteAccountRepository.execute(accountId)
+
+    if (!deletedAccount) {
+      throw new BadRequest('Failed to delete account')
+    }
+
+    return deletedAccount
+  }
+}
