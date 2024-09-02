f5c15c15b35cfe699b8bfab25adef19a30b857dc
Esdras Castro <ecff.dev@gmail.com>
2024-09-01 21:04:08 -0300

    feat: add GetAccountByIdService

---
+++ src/services/accounts/get-account-by-id.ts
@@ -0,0 +1,28 @@
+import { Account } from '@prisma/client'
+
+import { IGetAccountByIdRepository } from '@/repositories/accounts/get-account-by-id'
+import { BadRequest, NotFound } from '@/routes/_errors/errors-instance'
+
+export interface IGetAccountByIdService {
+  execute(id: string, userId: string): Promise<Account>
+}
+
+export class GetAccountByIdService implements IGetAccountByIdService {
+  constructor(private getAccountByIdRepository: IGetAccountByIdRepository) {}
+
+  async execute(id: string, userId: string) {
+    const account = await this.getAccountByIdRepository.execute(id)
+
+    if (!account) {
+      throw new NotFound('Account not found')
+    }
+
+    const userIdIsValid = account.userId == userId
+
+    if (!userIdIsValid) {
+      throw new BadRequest('Provided userId is not valid')
+    }
+
+    return account
+  }
+}
