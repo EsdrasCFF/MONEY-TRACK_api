b92db3a44b5f1c8cc6e6379d0497fc6700ad96ec
Esdras Castro <ecff.dev@gmail.com>
2024-09-05 23:55:24 -0300

    feat: add CreateTransactionService

---
+++ src/services/transactions/create-transactions.ts
@@ -0,0 +1,51 @@
+import { Transaction } from '@prisma/client'
+
+import { checkIfAmountIsValid, checkIfDateIsInFuture, convertAmountToHundredUnits } from '@/lib/utils'
+import { IGetAccountByIdRepository } from '@/repositories/accounts/get-account-by-id'
+import { CreateTransactionProps, ICreateTransactionRepository } from '@/repositories/transactions/create-transaction'
+import { IGetUserByIdRepository } from '@/repositories/users/get-user-by-id'
+import { BadRequest, NotFound } from '@/routes/_errors/errors-instance'
+
+export interface ICreateTransactionService {
+  execute(createTransactionParams: CreateTransactionProps, userId: string): Promise<Transaction>
+}
+
+export class CreateTransactionService implements ICreateTransactionService {
+  constructor(
+    private createTransactionRepository: ICreateTransactionRepository,
+    private getUserByIdRepository: IGetUserByIdRepository,
+    private getAccountByIdResository: IGetAccountByIdRepository
+  ) {}
+
+  async execute(createTransactionParams: CreateTransactionProps, userId: string) {
+    const userExists = this.getUserByIdRepository.execute(userId)
+
+    if (!userExists) {
+      throw new NotFound('User not found!')
+    }
+
+    const dateIsValid = !checkIfDateIsInFuture(createTransactionParams.date)
+
+    if (!dateIsValid) {
+      throw new BadRequest('Provided date cannot be in the future!')
+    }
+
+    const amountIsValid = checkIfAmountIsValid(String(createTransactionParams.amount))
+
+    if (!amountIsValid) {
+      throw new BadRequest('Provided amount is not valid!')
+    }
+
+    const accountExists = await this.getAccountByIdResository.execute(createTransactionParams.accountId)
+
+    if (!accountExists) {
+      throw new BadRequest('Account not Found! Provided id is not valid')
+    }
+
+    const transactionAmount = convertAmountToHundredUnits(createTransactionParams.amount, createTransactionParams.type)
+
+    const transaction = await this.createTransactionRepository.execute({ ...createTransactionParams, amount: transactionAmount })
+
+    return transaction
+  }
+}
