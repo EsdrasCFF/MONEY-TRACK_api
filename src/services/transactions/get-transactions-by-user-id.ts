fe80d18a78c4cb127d649ddc857306c056ce421b
Esdras Castro <ecff.dev@gmail.com>
2024-09-11 01:48:47 -0300

    feat: add GetTransactionsByUserIdService

---
+++ src/services/transactions/get-transactions-by-user-id.ts
@@ -0,0 +1,29 @@
+import { Transaction } from '@prisma/client'
+
+import { IGetTransactionsByUserIdRepository } from '@/repositories/transactions/get-transactions-by-user-id'
+
+import { IGetUserByIdRepository } from '../../repositories/users/get-user-by-id'
+import { NotFound } from '../../routes/_errors/errors-instance'
+
+export interface IGetTransactionsByUserIdService {
+  execute(userId: string): Promise<Transaction[]>
+}
+
+export class GetTransactionsByUserIdService implements IGetTransactionsByUserIdService {
+  constructor(
+    private getTransactionsByUserIdRepository: IGetTransactionsByUserIdRepository,
+    private getUserByIdRepository: IGetUserByIdRepository
+  ) {}
+
+  async execute(userId: string) {
+    const userExists = await this.getUserByIdRepository.execute(userId)
+
+    if (!userExists) {
+      throw new NotFound('USER_NOT_FOUND!')
+    }
+
+    const transactions = await this.getTransactionsByUserIdRepository.execute(userId)
+
+    return transactions
+  }
+}
