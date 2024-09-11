9f4ffb2abb74e3329d14334d33e9bf1be042895c
Esdras Castro <ecff.dev@gmail.com>
2024-09-11 01:50:41 -0300

    feat: add GetTransactionsByUserIdController

---
+++ src/controllers/transactions/get-transactions-by-user-id.ts
@@ -0,0 +1,23 @@
+import { Transaction } from '@prisma/client'
+
+import { IGetTransactionsByUserIdService } from '@/services/transactions/get-transactions-by-user-id'
+
+import { BadRequest } from '../../routes/_errors/errors-instance'
+
+export interface IGetTransactionsByUserIdController {
+  execute(userId: string): Promise<Transaction[]>
+}
+
+export class GetTransactionsByUserIdController implements IGetTransactionsByUserIdController {
+  constructor(private getTransactionsByUserIdService: IGetTransactionsByUserIdService) {}
+
+  async execute(userId: string) {
+    if (!userId) {
+      throw new BadRequest('User Id is missing or not provided!')
+    }
+
+    const transactions = await this.getTransactionsByUserIdService.execute(userId)
+
+    return transactions
+  }
+}
