8accae9791555cac1fa69f48ccb35faeaf04e32b
Esdras Castro <ecff.dev@gmail.com>
2024-09-11 01:46:53 -0300

    feat: add GetTransactionsByUserIdRepository

---
+++ src/repositories/transactions/get-transactions-by-user-id.ts
@@ -0,0 +1,21 @@
+import { Transaction } from '@prisma/client'
+
+import { db } from '../../lib/prisma'
+
+export interface IGetTransactionsByUserIdRepository {
+  execute: (userId: string) => Promise<Transaction[]>
+}
+
+export class GetTransactionsByUserIdRepository implements IGetTransactionsByUserIdRepository {
+  async execute(userId: string) {
+    const transactions = await db.transaction.findMany({
+      where: {
+        account: {
+          userId,
+        },
+      },
+    })
+
+    return transactions
+  }
+}
