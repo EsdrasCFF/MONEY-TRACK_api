c229e2d907988830c273d57dc30b2f0607bd6507
Esdras Castro <ecff.dev@gmail.com>
2024-09-15 17:57:32 -0300

    feat: add GetUserBalanceService

---
+++ src/services/users/get-user-balance.ts
@@ -0,0 +1,41 @@
+import { convertFromHundredUnitsToAmount } from '@/lib/utils'
+import { IGetUserBalanceRepository } from '@/repositories/users/get-user-balance'
+import { IGetUserByIdRepository } from '@/repositories/users/get-user-by-id'
+import { NotFound } from '@/routes/_errors/errors-instance'
+
+export interface BalanceParams {
+  incomes: number
+  expenses: number
+  investments: number
+  balance: number
+}
+
+export interface IGetUserBalanceService {
+  execute(userId: string, from: string, to: string): Promise<BalanceParams>
+}
+
+export class GetUserBalanceService implements IGetUserBalanceService {
+  constructor(
+    private getUserBalanceRepository: IGetUserBalanceRepository,
+    private getUserByIdRepository: IGetUserByIdRepository
+  ) {}
+
+  async execute(userId: string, from: string, to: string) {
+    const userExists = await this.getUserByIdRepository.execute(userId)
+
+    if (!userExists) {
+      throw new NotFound('User not found!')
+    }
+
+    const result = await this.getUserBalanceRepository.execute(userId, from, to)
+
+    const balance = {
+      incomes: convertFromHundredUnitsToAmount(result.incomes),
+      expenses: convertFromHundredUnitsToAmount(result.expenses),
+      investments: convertFromHundredUnitsToAmount(result.investments),
+      balance: convertFromHundredUnitsToAmount(result.balance),
+    }
+
+    return balance
+  }
+}
