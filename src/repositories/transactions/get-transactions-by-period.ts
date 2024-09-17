b061b7044d234a1d4f16838ae7218cc323bd26c9
Esdras Castro <ecff.dev@gmail.com>
2024-09-17 14:45:16 -0300

    feat: add GetTransactionsByPeriodRepository

---
+++ src/repositories/transactions/get-transactions-by-period.ts
@@ -0,0 +1,36 @@
+import { Prisma } from '@prisma/client'
+
+import { db } from '@/lib/prisma'
+
+type TransactionsWithDetails = Prisma.TransactionGetPayload<{
+  include: {
+    account: true
+    category: true
+  }
+}>
+
+export interface IGetTransactionsByPeriodRepository {
+  execute(from: Date, to: Date, userId: string): Promise<TransactionsWithDetails[]>
+}
+
+export class GetTransactionByPeriodRepository implements IGetTransactionsByPeriodRepository {
+  async execute(from: Date, to: Date, userId: string) {
+    const transactions = await db.transaction.findMany({
+      include: {
+        account: true,
+        category: true,
+      },
+      where: {
+        date: {
+          gte: from,
+          lte: to,
+        },
+        account: {
+          userId,
+        },
+      },
+    })
+
+    return transactions
+  }
+}
