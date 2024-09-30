6e6725bd895579a5d1a930e91e3d7f42a4d9dc3e
Esdras Castro <ecff.dev@gmail.com>
2024-09-30 15:17:17 -0300

    feat: add ShareAccountService

---
+++ src/services/accounts/share-account.ts
@@ -0,0 +1,53 @@
+import { UserAccount } from '@prisma/client'
+
+import { IGetAccountByIdRepository } from '@/repositories/accounts/get-account-by-id'
+import { IShareAccountRepository } from '@/repositories/accounts/share-account'
+import { IGetUserByEmailRepository } from '@/repositories/users/get-user-by-email'
+import { BadRequest, Forbidden, NotFound } from '@/routes/_errors/errors-instance'
+
+interface ShareAccountProps {
+  authenticatedUserId: string
+  userId: string
+  email: string
+  canEdit: boolean
+  canCreate: boolean
+  accountId: string
+}
+
+export interface IShareAccountService {
+  execute(shareAccountParams: ShareAccountProps): Promise<UserAccount>
+}
+
+export class ShareAccountService {
+  constructor(
+    private shareAccountRepository: IShareAccountRepository,
+    private getAccountByIdRepository: IGetAccountByIdRepository,
+    private getUserByEmailRepository: IGetUserByEmailRepository
+  ) {}
+
+  async execute({ authenticatedUserId, canCreate, canEdit, email, userId, accountId }: ShareAccountProps) {
+    const accountExists = await this.getAccountByIdRepository.execute(accountId)
+
+    if (!accountExists) {
+      throw new NotFound('Account not found')
+    }
+
+    if (authenticatedUserId !== accountExists.ownerId) {
+      throw new Forbidden('You do not have access to authorize this user!')
+    }
+
+    const userExists = await this.getUserByEmailRepository.execute(email)
+
+    if (!userExists) {
+      throw new NotFound('User not found')
+    }
+
+    if (userExists.id !== userId) {
+      throw new BadRequest('provided userId is invalid')
+    }
+
+    const accountShared = await this.shareAccountRepository.execute({ accountId, userId, canCreate, canEdit })
+
+    return accountShared
+  }
+}
