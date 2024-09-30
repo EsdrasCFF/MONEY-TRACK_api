2e45e3f716c3ee3a41aff5171a06fc5ced8aa51e
Esdras Castro <ecff.dev@gmail.com>
2024-09-29 23:08:07 -0300

    feat: add GetUserByEmailService

---
+++ src/services/users/get-user-by-email.ts
@@ -0,0 +1,40 @@
+import { IGetUserByEmailRepository } from '@/repositories/users/get-user-by-email'
+import { IGetUserByIdRepository } from '@/repositories/users/get-user-by-id'
+import { NotFound } from '@/routes/_errors/errors-instance'
+
+interface UserProps {
+  email: string
+  firstName: string
+  lastName: string
+}
+
+export interface IGetUserByEmailService {
+  execute(email: string, userId: string): Promise<UserProps>
+}
+
+export class GetUserByEmailService {
+  constructor(
+    private getUserByIdRepository: IGetUserByIdRepository,
+    private getUserByEmailRepository: IGetUserByEmailRepository
+  ) {}
+
+  async execute(email: string, userId: string) {
+    const userExists = await this.getUserByIdRepository.execute(userId)
+
+    if (!userExists) {
+      throw new NotFound('User not found')
+    }
+
+    const userByEmail = await this.getUserByEmailRepository.execute(email)
+
+    if (!userByEmail) {
+      throw new NotFound('User not found')
+    }
+
+    return {
+      firstName: userByEmail.firstName,
+      lastName: userByEmail.lastName,
+      email: userByEmail.email,
+    }
+  }
+}
